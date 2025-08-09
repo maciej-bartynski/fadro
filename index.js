218900bac04088b8ab11ea677b0a645a8b386f51
Maciej Bartyński <maciek.bartynski@gmail.com>
2025-08-09 12:18:52 +0200

    initializing

---
+++ index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+
+function copyDirContent(from, to) {
+    const sourceDir = path.join(__dirname, from);
+    const destDir = path.join(process.cwd(), to);
+
+    if (!fs.existsSync(destDir)) {
+        fs.mkdirSync(destDir, { recursive: true });
+    }
+
+    fs.readdirSync(sourceDir).forEach(file => {
+        const src = path.join(sourceDir, file);
+        const dest = path.join(destDir, file);
+        fs.copyFileSync(src, dest);
+    });
+}
+
+function copyConfig() {
+    const configName = 'cursorignore';
+    const cursorignore = path.join('config', configName);
+    fs.copyFileSync(cursorignore, `.${configName}`);
+}
+
+function addToGitignore() {
+    const gitignorePath = path.join(process.cwd(), '.gitignore');
+    const entryToAdd = 'docs/task-log.md';
+
+    let content = '# Fadro ignore\n';
+
+    if (fs.existsSync(gitignorePath)) {
+        content = fs.readFileSync(gitignorePath, 'utf8');
+    } else {
+        return;
+    }
+
+    const lines = content.split(/\r?\n/);
+    if (!lines.includes(entryToAdd)) {
+        const newContent = content.endsWith('\n') ? content + entryToAdd + '\n' : content + '\n' + entryToAdd + '\n';
+        fs.writeFileSync(gitignorePath, newContent, 'utf8');
+    }
+}
+
+copyDirContent('rules', '.cursor/rules');
+copyDirContent('docs', 'docs');
+copyConfig();
+addToGitignore();
+
+console.log(`✔ Fadro ready to work`);
